b63c522c0c887e43608d25d07c6b8fbe981db49d
hbhungg <hung.ba.huynh@proton.me>
2025-05-18 19:35:31 +1000

    no ignore everything

---
+++ nvim/.config/nvim.bak/colors/my-peachpuff.vim
@@ -0,0 +1,97 @@
+" My version of peachpuff with tree-sitter
+
+let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+let &t_8b = "\<Esc>[48:2;%lu;%lu;%lum"
+
+hi clear
+if exists("syntax_on")
+  syntax reset
+endif
+let colors_name = "my-peachpuff"
+
+" hi SpecialKey term=bold ctermfg=4 guifg=Blue
+hi NonText term=bold cterm=bold ctermfg=4
+hi Directory term=bold ctermfg=4
+hi ErrorMsg term=standout cterm=bold ctermfg=7 ctermbg=1
+hi IncSearch term=reverse cterm=reverse
+hi Search ctermfg=Red ctermbg=LightYellow
+hi MoreMsg term=bold ctermfg=2
+hi ModeMsg term=bold cterm=bold ctermfg=White
+hi LineNr term=underline ctermfg=3
+hi Question term=standout ctermfg=2
+hi StatusLine term=bold,reverse cterm=bold,reverse
+hi StatusLineNC term=reverse cterm=reverse
+hi VertSplit term=reverse cterm=reverse
+hi WinSeparator ctermfg=Black ctermbg=White
+hi Title term=bold ctermfg=5
+hi Visual ctermfg=Black ctermbg=White
+hi VisualNOS term=bold,underline cterm=bold,underline
+hi WarningMsg term=standout ctermfg=1
+hi WildMenu term=standout ctermfg=0 ctermbg=3
+hi Folded term=standout ctermfg=4 ctermbg=7
+hi FoldColumn term=standout ctermfg=4 ctermbg=7
+hi DiffAdd term=bold ctermbg=4
+hi DiffChange term=bold ctermbg=5
+hi DiffDelete term=bold cterm=bold ctermfg=4 ctermbg=6
+hi DiffText term=reverse cterm=bold ctermbg=1
+hi Cursor guifg=bg guibg=fg
+hi lCursor guifg=bg guibg=fg
+
+" Colors for syntax highlighting
+hi Comment term=bold ctermfg=4
+hi Constant term=underline ctermfg=1
+hi Special ctermfg=DarkCyan
+" hi Identifier term=underline ctermfg=6 guifg=DarkCyan
+hi Statement term=bold ctermfg=3
+hi PreProc term=underline ctermfg=5
+hi Type term=underline ctermfg=2
+hi Ignore cterm=bold ctermfg=7
+hi Error term=reverse cterm=bold ctermfg=7 ctermbg=1
+
+" Tree-sitter highlight. (IDK why i havent move over to vscode, cause setting this up is a pain)
+" Misc
+hi @punctuation ctermfg=White
+" Python block comment
+hi @comment ctermfg=DarkGray
+hi @spell ctermfg=DarkGray
+hi @string.special.url.comment ctermfg=DarkGray
+hi @string.documentation ctermfg=DarkGray
+hi @nospell ctermfg=DarkGray cterm=underline,bold
+
+" Highlight for special comment tags
+hi @text.note cterm=bold ctermfg=Black ctermbg=LightBlue
+
+" Function, class and method
+hi @function ctermfg=DarkCyan
+hi @function.call ctermfg=DarkCyan
+hi @function.builtin ctermfg=DarkCyan
+hi @variable.builtin.python ctermfg=White
+hi @constructor ctermfg=DarkCyan
+hi @method ctermfg=DarkCyan
+hi @attribute ctermfg=DarkCyan
+hi @attribute.python ctermfg=DarkCyan
+
+" Constant
+hi @boolean ctermfg=Magenta
+hi @constant ctermfg=Magenta
+hi @namespace ctermfg=DarkCyan
+
+" Variable
+hi @variable ctermfg=White
+hi @field ctermfg=White
+hi @property ctermfg=White
+hi @type ctermfg=DarkGreen
+hi @type.builtin ctermfg=DarkGreen
+hi @string ctermfg=Brown
+
+" LSP
+hi DiagnosticError cterm=underline ctermfg=Red
+hi DiagnosticWarn cterm=underline ctermfg=Yellow
+hi DiagnosticInfo cterm=underline ctermfg=Blue
+hi DiagnosticHint cterm=underline ctermfg=DarkGreen
+hi DiagnosticUnnecessary ctermfg=DarkGray
+
+" hi! LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+" hi! LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+" hi! LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+hi! link SignColumn Normal
---
+++ nvim/.config/nvim.bak/lua/lsp.lua
@@ -0,0 +1,135 @@
+-- LSP --
+-- Turn off all of the diagnostic
+-- We only need the jumping capability
+-- vim.diagnostic.disable()
+
+vim.opt.signcolumn='auto'
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+  float = { border = 'rounded' },
+})
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'rounded'})
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'rounded'})
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  if client.name == 'ruff_lsp' then
+    -- Disable hover in favor of Pyright
+    client.server_capabilities.hoverProvider = false
+  end
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>j', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>j', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>fm', '<cmd>lua vim.lsp.buf.format({async = true})<CR>', opts)
+end
+
+-- Autocmp --
+local cmp = require'cmp'
+
+vim.o.completeopt = 'menu,menuone,noselect'
+
+cmp.setup({
+  -- completion = { autocomplete = false },
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'gopls', 'rust_analyzer', 'ccls' , 'ts_ls' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+     on_attach = on_attach,
+     flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+      capabilities = capabilities
+     }
+  }
+end
+
+-- require('lspconfig').ruff_lsp.setup {
+--   on_attach = on_attach,
+-- }
+require('lspconfig').pyright.setup {
+  on_attach = on_attach,
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        diagnosticSeverityOverrides = {
+          reportPrivateImportUsage = "none",
+        }
+      }
+    }
+  },
+}
