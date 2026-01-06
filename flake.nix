61fdd1bd93e25bc8622263cba13d5cb42d812ea6
hbhungg <hung.ba.huynh@proton.me>
2026-01-06 21:03:26 +0700

    home manager and git

--- flake.nix
+++ flake.nix
@@ -5,9 +5,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
       programs.bash.enable = true;
@@ -50,6 +52,68 @@
         ];
       };
 
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+	backupFileExtension = "backup";
+	extraSpecialArgs = { inherit inputs; };
+      };
+      home-manager.users.wren = { pkgs, ... }: {
+	home.stateVersion = "25.11";
+
+	programs.bash.enable = true;
+	programs.htop.enable = true;
+	programs.htop.settings.show_program_path = true;
+
+	home.packages = [ ];
+
+	programs.git.enable = true;
+	programs.git.settings = {
+          user.name = "hbhungg";
+          user.email = "hung.ba.huynh@proton.me";
+	  alias = {
+            st = "status";
+            lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+            p = "push";
+            cn = "commit";
+            c = "commit";
+          };
+
+          core = {
+	    editor = "nvim";
+            compression = 9;
+            preloadindex = true;
+            ignorecase = false;
+	  };
+	  init.defaultBranch = "main";
+	  column.ui = "auto";
+	  status = {
+            branch = true;
+            showStash = true;
+            showUntrackedFiles = "all";
+          };
+	  commit.verbose = true;
+	  push = {
+            default = "simple";
+            autoSetupRemote = true;
+          };
+	  diff = {
+            algorithm = "histogram";
+            colorMoved = "plain";
+            mnemonicPrefix = true;
+            renames = true;
+          };
+	  branch.sort = "-committerdate";
+          tag.sort = "version:refname";
+	  filter."lfs" = {
+            smudge = "git-lfs smudge -- %f";
+            process = "git-lfs filter-process";
+            required = true;
+            clean = "git-lfs clean -- %f";
+          };
+        };
+      };
+
       system.defaults = {
         dock.autohide = true;
         dock.persistent-apps = [
@@ -78,7 +142,10 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#nest-2
     darwinConfigurations."nest-2" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration
+        home-manager.darwinModules.home-manager
+      ];
     };
   };
 }
