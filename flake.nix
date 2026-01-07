23895aff3a20af266c6903b7514684cc0809e79b
hbhungg <hung.ba.huynh@proton.me>
2026-01-07 14:27:24 +0700

    add git delta

--- flake.nix
+++ flake.nix
@@ -167,12 +167,48 @@
             terminal = "xterm-256color";
             prefix = "`";
             keyMode = "vi";
-            extraConfig = ''
+	    extraConfig = ''
               set -g default-command "/run/current-system/sw/bin/bash"
               set -ag terminal-overrides ",xterm-256color:RGB"
               bind-key ` last-window
-              set -g status-style fg=color137,bg=default
-              # ... (rest of your tmux settings)
+              bind-key e send-prefix
+              bind-key b select-window -t:-1
+              bind c new-window -c "#{pane_current_path}"
+              bind-key = split-window -h -c "#{pane_current_path}"
+              bind-key - split-window -v -c "#{pane_current_path}"
+              set -g set-titles on
+              set -g status-interval 1
+              set -g focus-events on
+              set -g status-justify left
+              set -g status-position bottom
+              set-window-option -g window-status-separator '  '
+              set-option -g status-style fg=color137,bg=default
+              set -g status-left ' '
+              set -g status-right '#H:#S #[fg=colour255,bg=default] #(tmux-mem-cpu-load --interval 1 -a 0 -g 0)  %I:%M %p '
+              set -g status-right-length 140
+              set -g status-left-length 20
+              setw -g window-status-current-format '#I#[fg=colour250]:#[fg=colour255]#W#[fg=colour50]#F'
+              setw -g window-status-format '#[fg=colour240]#I:#W#[fg=colour255]#F'
+              set-option -g automatic-rename on
+
+              is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+                  | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+              bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+              bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+              bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+              bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+
+              tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+              if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+                  "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
+              if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+                  "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+
+              bind-key -T copy-mode-vi 'C-h' select-pane -L
+              bind-key -T copy-mode-vi 'C-j' select-pane -D
+              bind-key -T copy-mode-vi 'C-k' select-pane -U
+              bind-key -T copy-mode-vi 'C-l' select-pane -R
+              bind-key -T copy-mode-vi 'C-\' select-pane -l
             '';
             plugins = with pkgs.tmuxPlugins; [ vim-tmux-navigator ];
           };
@@ -181,7 +217,6 @@
             EDITOR = "nvim"; 
             VISUAL = "nvim";
             LSCOLORS = "GxBxhxDxfxhxhxhxhxcxcx";
-            # Cyan (36) for directories to match /bin/ls
             LS_COLORS = "di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43";
             TERM = "xterm-256color";
           };
@@ -192,6 +227,18 @@
           };
 
           programs.git.enable = true;
+	  programs.delta = {
+	    enable = true;
+	    enableGitIntegration = true;
+	    options = {
+              features = "side-by-side";
+              syntax-theme = "ansi";
+              paging = "always";
+              pager = "less";
+              file-style = "red bold ul";
+              file-decoration-style = "blue box"; 
+            };
+	  };
           programs.git.settings = {
             user.name = "hbhungg";
             user.email = "hung.ba.huynh@proton.me";
@@ -201,6 +248,7 @@
               p = "push";
               cn = "commit";
               c = "commit";
+	      d = "diff";
             };
             core = {
               editor = "nvim";
