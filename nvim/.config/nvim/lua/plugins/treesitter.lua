da330a1a7dea7981b7c084165557426887a5e998
hbhungg <hung.ba.huynh@proton.me>
2026-05-22 17:51:10 +1000

    new treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,10 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
+  'romus204/tree-sitter-manager.nvim',
   opts = {
     ensure_installed = {
-      'bash',
-      'c',
-      'diff',
       'html',
-      'lua',
       'luadoc',
-      'markdown',
       'markdown_inline',
-      'query',
-      'vim',
-      'vimdoc',
       'typescript',
       'python',
       'java',
@@ -22,11 +12,6 @@ return {
       'hcl',
     },
     auto_install = true,
-    highlight = {
-      enable = true,
-      -- Ruby's indent rules depend on vim's regex highlighter.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = false, disable = { 'ruby' } },
+    highlight = true,
   },
 }
